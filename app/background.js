7a9ebffa127dc6c97c654e3c35c6c0c24462d4a1
Mitchell Taylor <mtaylor22@neo.tamu.edu>
2014-03-24 03:52:48 -0500

    Chrome ext. badge

--- app/background.js
+++ app/background.js
@@ -2,10 +2,15 @@ function checkNotification(){
 	jQuery.getJSON("http://crowdref.atwebpages.com/mobile_login.php", function(data) {
 	    if (data.status == "1"){
 	    	jQuery.getJSON("http://crowdref.atwebpages.com/mobile_get_notifications.php", function(notif) {
+	    		chrome.browserAction.setBadgeBackgroundColor({color: [80, 0, 0, 255] });
       			chrome.browserAction.setBadgeText({text: notif.count});
+      			chrome.browserAction.setTitle({"title": "CroudRef: " + notif.count + " new references"});
 	    	});
 	    } else {
 	    	//still not logged in 
+	    		chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 255] });
+      			chrome.browserAction.setBadgeText({"text": "!"});
+      			chrome.browserAction.setTitle({"title": "Please Login into CrowdRef"});
 	    }
 	});
 }
